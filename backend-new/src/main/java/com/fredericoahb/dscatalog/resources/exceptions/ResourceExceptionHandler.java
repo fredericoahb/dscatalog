efd3a77402ff8e34cb42b68737050b271b1bda81
Frederico Balieiro <fredericoahb@gmail.com>
2020-12-24 23:33:45 -0300

    Integrate product listing with backend

---
+++ backend-new/src/main/java/com/fredericoahb/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.fredericoahb.dscatalog.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.fredericoahb.dscatalog.services.exceptions.DatabaseException;
+import com.fredericoahb.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError();
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not Found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		StandardError err = new StandardError();
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+	
+	
+}
---
+++ backend-new/src/test/java/com/fredericoahb/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredericoahb.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
